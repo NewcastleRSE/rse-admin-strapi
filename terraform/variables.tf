654168ec03fb830afbf15c9d35ca2811e23b6a5b
markdturner <mark.turner@newcastle.ac.uk>
2022-03-29 09:43:21 +0100

    updated terraform variables to account for new DB

---
+++ terraform/variables.tf
@@ -0,0 +1,72 @@
+variable "resource_group_name" {
+  default = "rseadmin"
+}
+
+variable "resource_group_location" {
+  default = "uksouth"
+}
+
+variable "project_name" {
+  type = string
+  default = "RSE Admin"
+}
+
+variable "project_pi" {
+  type = string
+  default = "Mark Turner"
+}
+
+variable "project_contributors" {
+  type = string
+  default = "Mark Turner"
+}
+
+variable "app_keys" {
+  type        = string
+  sensitive   = true
+}
+
+variable "jwt_secret" {
+  type        = string
+  sensitive   = true
+}
+
+variable "api_token_salt" {
+  type        = string
+  sensitive   = true
+}
+
+variable "database_host" {
+  type        = string
+  sensitive   = true
+}
+
+variable "database_port" {
+  type        = string
+  sensitive   = true
+}
+
+variable "database_name" {
+  type        = string
+  sensitive   = true
+}
+
+variable "database_username" {
+  type        = string
+  sensitive   = true
+}
+
+variable "database_password" {
+  type        = string
+  sensitive   = true
+}
+
+variable "database_ssl" {
+  type        = string
+  sensitive   = true
+}
+
+variable "hubspot_key" {
+  type        = string
+  sensitive   = true
+}
