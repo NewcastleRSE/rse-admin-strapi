8a1c008eb9ad95d7624567c72e75fb843b048f63
markdturner <mark.turner@newcastle.ac.uk>
2022-08-10 22:31:11 +0100

    parse excel file and iterate over the rows

--- src/api/transaction/controllers/transaction.js
+++ src/api/transaction/controllers/transaction.js
@@ -10,7 +10,14 @@ module.exports = createCoreController('api::transaction.transaction', ({ strapi 
     async upload(ctx) {
         const file = ctx.request.files['files.file']
 
-        const entity = await strapi.service('api::transaction.transaction').upload(file);
+        let entity = {
+          error: 'Incorrect MIME Type. Please upload an Excel Spreadsheet'
+        }
+
+        if(file.type = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
+          entity = await strapi.service('api::transaction.transaction').upload(file)
+        }
+
         const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
 
         return this.transformResponse(sanitizedEntity);
--- src/api/transaction/services/transaction.js
+++ src/api/transaction/services/transaction.js
@@ -5,10 +5,20 @@
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
+const ExcelJS = require('exceljs')
 
 module.exports = createCoreService('api::transaction.transaction', ({ strapi }) =>  ({
     async upload(file) {
-        console.log(file)
-        return { message: 'OK' }
+        const workbook = new ExcelJS.Workbook()
+        await workbook.xlsx.readFile(file.path)
+        const worksheet = workbook.getWorksheet('Transactions All Time')
+
+        let transactions = []
+
+        worksheet.eachRow(function(row, rowNumber) {
+            transactions.push(row.values)
+        })
+        console.log(transactions)
+        return { transactions }
       },
 }))
