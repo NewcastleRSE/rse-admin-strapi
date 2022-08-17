e808fdcace6d2adb0dee424af8bf32d6cafc87a9
markdturner <mark.turner@newcastle.ac.uk>
2022-08-17 12:14:02 +0100

    rebuilt facility data model

--- src/api/facility-year/controllers/facility-year.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  facility-year controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::facility-year.facility-year');
--- src/api/facility-year/services/facility-year.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * facility-year service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::facility-year.facility-year');
