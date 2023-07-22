e1f7492a34aa72fa2d8432503a00eb3fbe280918
Mark Turner <mark.turner@ncl.ac.uk>
2023-07-22 22:11:40 +0100

    complete PDF form with know values

--- src/api/invoice/controllers/invoice.js
+++ src/api/invoice/controllers/invoice.js
@@ -8,12 +8,17 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::invoice.invoice', ({ strapi }) =>  ({
     async create(ctx) {
+
+      const projectId = ctx.request.body.data.project,
+            year = ctx.request.body.data.year,
+            month = ctx.request.body.data.month
+
       try {
         const pdfData = await strapi.service("api::invoice.invoice").create(ctx.request.body)
         ctx.body = pdfData
         ctx.type = 'application/pdf'
         ctx.set('Content-Type', 'application/pdf')
-        ctx.set('Content-disposition', `attachment;filename=docname.pdf`)
+        ctx.set('Content-disposition', `attachment;filename=${projectId}-${year}-${month}.pdf`)
       } catch (err) {
         console.error(err)
         ctx.body = err;
--- src/api/invoice/services/invoice.js
+++ src/api/invoice/services/invoice.js
@@ -2,6 +2,7 @@
 
 const { DateTime } = require('luxon')
 const { PDFDocument } = require('pdf-lib')
+const fontKit = require ('@pdf-lib/fontkit')
 const fs = require('fs/promises')
 const path = require('path')
 
@@ -13,15 +14,74 @@ const { createCoreService } = require('@strapi/strapi').factories;
 
 module.exports = createCoreService('api::invoice.invoice', ({ strapi }) => ({
     async create(params) {
+        const project = await strapi.service("api::project.project").findOne(params.data.project)
+
+        console.log(project)
+
+        params.data.project = [project.id]
         params.data.generated = DateTime.utc().toISODate()
+
         await super.create(params)
+
+        const formatter = new Intl.NumberFormat('en-GB', {
+            style: 'currency',
+            currency: 'GBP',
+        });
 
         const pdfData = await fs.readFile(path.resolve(__dirname, './ir-template.pdf'))
         const pdfDoc = await PDFDocument.load(pdfData)
+
+        pdfDoc.registerFontkit(fontKit)
+        const fontBytesBold = await fs.readFile(path.resolve(__dirname, './arial-black.otf'))
+        const fontBold = await pdfDoc.embedFont(fontBytesBold)
+
+        const form = pdfDoc.getForm()
+
+        const sapDocument = form.getTextField('SAP Document')
+        sapDocument.updateAppearances(fontBold)
+        sapDocument.setText(`${project.hubspotId}-${params.data.year}-${params.data.month}`)
+        sapDocument.enableReadOnly()
+
+        const refNumber = form.getTextField('REF Number')
+        refNumber.updateAppearances(fontBold)
+        refNumber.setText(`${project.hubspotId}`)
+        refNumber.enableReadOnly()
+
+        const created = form.getTextField('Created')
+        created.updateAppearances(fontBold)
+        created.setText(`${params.data.generated}`)
+        created.enableReadOnly()
+
+        const enteredBy = form.getTextField('Entered By')
+        enteredBy.updateAppearances(fontBold)
+        enteredBy.setText(`Mark Turner`)
+        enteredBy.enableReadOnly()
+
+        const description = form.getTextField('Description')
+        description.updateAppearances(fontBold)
+        description.setText(`RSE services for ${project.dealname}.`)
+        description.enableReadOnly()
+
+        const quantity = form.getTextField('Quantity')
+        quantity.updateAppearances(fontBold)
+        quantity.setText(`100`)
+        quantity.enableReadOnly()
+
+        const price = form.getTextField('Price')
+        price.updateAppearances(fontBold)
+        price.setText(`${formatter.format(392.23)}`)
+        price.enableReadOnly()
+
+        const total = form.getTextField('Total')
+        total.updateAppearances(fontBold)
+        total.setText(`${formatter.format(392.23 * 100)}`)
+        total.enableReadOnly()
+
+        const account = form.getTextField('Account')
+        account.updateAppearances(fontBold)
+        account.setText(`${project.accountCode}`)
+        account.enableReadOnly()
         
-        const pages = pdfDoc.getPages()
-        pages[0].drawText('You can modify PDFs too!')
-        
-        return Buffer.from(await pdfDoc.save({ useObjectStreams: false }))
+        return Buffer.from(await pdfDoc.save())
     }
 }))
