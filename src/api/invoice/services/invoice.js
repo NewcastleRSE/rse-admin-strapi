ff79ea0ada0deb3f943cb9c09a4ab2ee0cad49ad
Mark Turner <mark.turner@ncl.ac.uk>
2023-07-22 13:56:45 +0100

    generate invoice with current date

--- src/api/invoice/services/invoice.js
+++ src/api/invoice/services/invoice.js
@@ -1,9 +1,16 @@
 'use strict';
 
+const { DateTime } = require("luxon")
+
 /**
  * invoice service
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::invoice.invoice');
+module.exports = createCoreService('api::invoice.invoice', ({ strapi }) => ({
+    async create(params) {
+        params.data.generated = DateTime.utc().toISODate()
+        return await super.create(params)
+    }
+}))
