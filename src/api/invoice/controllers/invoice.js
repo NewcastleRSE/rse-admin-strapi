60e52279b44d45138bad13ec621e9b966ad109e5
Mark Turner <mark.turner@ncl.ac.uk>
2023-07-22 20:09:17 +0100

    download invoice pdf

--- src/api/invoice/controllers/invoice.js
+++ src/api/invoice/controllers/invoice.js
@@ -6,4 +6,18 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::invoice.invoice');
+module.exports = createCoreController('api::invoice.invoice', ({ strapi }) =>  ({
+    async create(ctx) {
+      try {
+        const pdfData = await strapi.service("api::invoice.invoice").create(ctx.request.body)
+        ctx.body = pdfData
+        ctx.type = 'application/pdf'
+        ctx.set('Content-Type', 'application/pdf')
+        ctx.set('Content-disposition', `attachment;filename=docname.pdf`)
+      } catch (err) {
+        console.error(err)
+        ctx.body = err;
+      }
+    }
+})
+);
--- src/api/invoice/services/invoice.js
+++ src/api/invoice/services/invoice.js
@@ -1,6 +1,9 @@
 'use strict';
 
-const { DateTime } = require("luxon")
+const { DateTime } = require('luxon')
+const { PDFDocument } = require('pdf-lib')
+const fs = require('fs/promises')
+const path = require('path')
 
 /**
  * invoice service
@@ -11,6 +14,14 @@ const { createCoreService } = require('@strapi/strapi').factories;
 module.exports = createCoreService('api::invoice.invoice', ({ strapi }) => ({
     async create(params) {
         params.data.generated = DateTime.utc().toISODate()
-        return await super.create(params)
+        await super.create(params)
+
+        const pdfData = await fs.readFile(path.resolve(__dirname, './ir-template.pdf'))
+        const pdfDoc = await PDFDocument.load(pdfData)
+        
+        const pages = pdfDoc.getPages()
+        pages[0].drawText('You can modify PDFs too!')
+        
+        return Buffer.from(await pdfDoc.save({ useObjectStreams: false }))
     }
 }))
