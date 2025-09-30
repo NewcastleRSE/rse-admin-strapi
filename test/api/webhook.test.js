f37a02e106f781ad8f4380cd118e62efae35716a
Mark Turner <mark.turner@ncl.ac.uk>
2025-09-30 20:56:07 +0100

    webhook test file

---
+++ test/api/webhook.test.js
@@ -0,0 +1,21 @@
+const request = require('supertest')
+
+let token
+
+beforeAll(async () => {
+  await request(strapi.server.httpServer)
+        .post('/api/auth/local')
+        .set('accept', 'application/json')
+        .set('Content-Type', 'application/json')
+        .send({
+          identifier: 'tester@strapi.com',
+          password: '1234abcd',
+        })
+        .then((data) => {
+          JWT = data.body.jwt
+        })
+})
+
+describe('Webhook API', () => {
+
+})
